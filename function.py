ffe5c78e610de793ce1b79cf47ee7948dfc1e1ee
AbdulKadir O. Oyedeji <oyedeji.oyeniyi@outlook.com>
2024-10-19 13:12:39 +0100

    Function is refined, now a general player with function call

--- function.py
+++ function.py
@@ -1,29 +1,26 @@
-print("""Choose from available Surah
-      1. Baqarah
-      2. A\'ma 
-      3. Mulku""")
 
+Playtrack = {1:"Baqarah", 
+             2:"A\'ma", 
+             3:"Mulku"}
 
-choosen_num = int(input("Surah: "))
+[print(f"{key}:{value}") for key, value in Playtrack.items()]
+
 try:
-  if choosen_num == 1:
-    print("Baqarah Selected")
-  elif choosen_num == 2:
-    print("A\'ma  Selected")
-  elif choosen_num == 3:
-    print("Mulku  Selected")
-  else:
-    print("Choose from the Surah listed")
+  choosen_num = int(input("Choose from available Surah: "))
+  
+  verse_start = int(input("\nEnter from which verse to play: "))
+  verse_stop = int(input("\nEnter verse to stop play: "))
+  verse_range = [verse_start, verse_stop]
 except ValueError:
-  print("Enter the Surah number")
+    print("Enter the Surah number")
 
-verse_start = int(input("Enter from which verse to play: "))
-verse_stop = int(input("Enter verse to stop play: "))
-Playtrack = ["Baqarah", "A\'ma", "Mulku"]
+def play_audi(Surah, Verse1, Verse2):
+  for playlist in range(Verse1,Verse2+1):
+    print(f"\nPlaying {Surah} --> verse {playlist}")
+  
 
-
-def play_audi(Surah, Verse):
-  for i in range(1,10,2):
-    print(a+b)
-    
-playlist = play_audi(2,8)
+if choosen_num in Playtrack:
+  play_audi(Playtrack[choosen_num], *verse_range)
+else:
+  print("Choose from the Surah listed")
+ 
